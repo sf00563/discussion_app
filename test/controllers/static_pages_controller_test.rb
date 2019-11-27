a1cca11e77698d14486857963677c6cee1660c0b
Sam Foster <samf2398@gmail.com>
2019-11-27 15:08:52 +0000

    added some tests for static pages controller, made the title dynamic for both contact and home pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @after_bar_title = "Discussion"
+  end
+
   test "should get home" do
     get static_pages_home_url
+    #was request sucessful
     assert_response :success
+    #was correct layout used
+    assert_template layout: 'application'
+    #was the correct title retrieved
+    assert_select "title", "Home | #{@after_bar_title}"
   end
 
   test "should get contact" do
     get static_pages_contact_url
+    #was request sucessful
     assert_response :success
+    #was correct layout used
+    assert_template layout: 'application'
+    #was the correct title retrieved
+    assert_select "title", "Contact | #{@after_bar_title}"
   end
 
 end
