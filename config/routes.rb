d2c4feda4c7f8ce5a789c1856a9bc4c2d2f9fabf
Sam Foster <samf2398@gmail.com>
2019-11-27 15:30:28 +0000

    set home as default landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/contact'
+  root 'static_pages#home'
+  get 'home' => 'static_pages#home'
+  get 'contact' => 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
