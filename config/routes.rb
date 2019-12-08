ce2b62fad858c9a6a52cfee1a182641f6b25e66b
Sam Foster <samf2398@gmail.com>
2019-12-08 16:26:35 +0000

    added name field to users and created new registrations contoller to deal with that

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   root to: 'static_pages#home'
   get 'home' => 'static_pages#home'
   get 'contact' => 'static_pages#contact'
