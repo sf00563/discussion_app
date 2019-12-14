e835d04f5d8e2453e9a85eaa1d11af873e0110e4
Sam Foster <samf2398@gmail.com>
2019-12-14 16:26:42 +0000

    making the routes neater and putting link to instead of a tag

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   post 'contact' => 'static_pages#create', as: 'create_message'
   get 'discussiontopics' => 'discussiontopics#index'
-  get 'discussiontopics/:id' => 'discussiontopics#show'
-  get 'discussions/:id' => 'discussions#show'
-  get 'discussions/:id/edit' => 'discussions#edit'
+  get 'discussiontopics/:id' => 'discussiontopics#show', as: 'discussiontopics_discussions'
+  get 'discussions/:id' => 'discussions#show', as: 'discussion'
+  get 'discussions/:id/edit' => 'discussions#edit', as: 'edit_discussion'
   patch 'discussions/:id' => 'discussions#update'
-  get 'discussions/new/:discussiontopicid' => 'discussions#new'
+  get 'discussions/new/:discussiontopicid' => 'discussions#new', as: 'new_discussion'
   post "discussions" => "discussions#create"
   delete "discussions/:id" => "discussions#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
