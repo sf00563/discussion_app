efa702c6993d526f03fabaa8a128cb79d54e24d1
Sam Foster <samf2398@gmail.com>
2019-12-15 18:05:34 +0000

    added tests for discussion topic controller

--- test/controllers/discussiontopics_controller_test.rb
+++ test/controllers/discussiontopics_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class DiscussiontopicsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_url
+    @after_bar_title = "Discussion"
+  end
+
+  test "should get index" do
+    get discussiontopics_path
+    assert_response :success
+    assert_select "title", "Discussion Topic | #{@after_bar_title}"
+    assert_select "h1", "Discussion topics"
+  end
+
+  test "should get show" do
+    discussion = discussions(:validDiscussion)
+    get discussiontopics_discussions_path(discussion)
+    assert_response :success
+    assert_select "title", "Discussions | #{@after_bar_title}"
+    assert_select 'table' do
+      assert_select 'td',"Title"
+      assert_select 'td',"Brief description"
+      assert_select 'td',"Date created"
+      assert_select 'td',"Created by"
+      assert_select 'td',"Options"
+    end
+  end
+
 end
