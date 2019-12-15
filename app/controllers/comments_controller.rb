75d7597a248cca248e50246fa5b09591c85a2504
Sam Foster <samf2398@gmail.com>
2019-12-15 00:20:49 +0000

    added update edit and patch to the comment controller, can edit and create new comment now

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,41 @@
+class CommentsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    comment = Comment.new(comment_params)
+
+    if comment.save
+      redirect_to "/discussions/#{params[:comment][:discussion_id]}"
+    else
+      flash[:errors] = comment.errors.full_messages
+      redirect_back(fallback_location: root_path)
+    end
+
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+    if comment.update(update_comment_params)
+      redirect_to "/discussiontopics"
+    else
+      flash[:errors] = comment.errors.full_messages
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:comment,:user_id,:discussion_id)
+    end
+
+    def update_comment_params
+      params.require(:comment).permit(:comment)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
+
   root to: 'static_pages#home'
   get 'home' => 'static_pages#home'
   get 'contact' => 'static_pages#contact'
   post 'contact' => 'static_pages#create', as: 'create_message'
+
   get 'discussiontopics' => 'discussiontopics#index'
   get 'discussiontopics/:id' => 'discussiontopics#show', as: 'discussiontopics_discussions'
+
   get 'discussions/:id' => 'discussions#show', as: 'discussion'
   get 'discussions/:id/edit' => 'discussions#edit', as: 'edit_discussion'
   patch 'discussions/:id' => 'discussions#update'
   get 'discussions/new/:discussiontopicid' => 'discussions#new', as: 'new_discussion'
-  post "discussions" => "discussions#create"
-  delete "discussions/:id" => "discussions#destroy"
+  post 'discussions' => 'discussions#create'
+  delete 'discussions/:id' => 'discussions#destroy'
+
+  get '/comments/new/:discussionid' => 'comments#new'
+  post 'comments' => 'comments#create'
+  get 'comments/:id/edit' => 'comments#edit'
+  patch 'comments/:id' => 'comments#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
